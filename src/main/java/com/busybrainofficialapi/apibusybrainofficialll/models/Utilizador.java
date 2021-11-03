d64e6e14a828244ef58fcd3115538cd23026183d
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2021-11-03 00:30:14 +0000

    Add files via upload

---
+++ src/main/java/com/busybrainofficialapi/apibusybrainofficialll/models/Utilizador.java
@@ -0,0 +1,59 @@
+package com.busybrainofficialapi.apibusybrainofficialll.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity  
+@Table(name = "utilizador")
+public class Utilizador {
+
+    @Id  //Obrigatório possuir ID
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+    @Column(name = "user_id") private int id;
+    @Column(name = "user_username") private String username;
+    @Column(name = "user_email") private String email;
+    @Column(name = "user_password") private String password;
+
+    public Utilizador(){
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
---
+++ src/main/java/com/busybrainofficialapi/apibusybrainofficialll/models/UtilizadorRepository.java
@@ -0,0 +1,7 @@
+package com.busybrainofficialapi.apibusybrainofficialll.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UtilizadorRepository extends CrudRepository<Utilizador, Integer>{
+    
+}
